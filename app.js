f086d0fe9553912f81c0b7d8596df1abfc216aac
Alan Wei <alan.wei@live.com>
2016-03-27 00:10:16 +0800

    update

--- app.js
+++ app.js
@@ -11,8 +11,9 @@ angular.module("AlanAuthTestApp", [
 ]).run(function (AlanAuth, User) {
     User.role = "guest";
 
-    AlanAuth.setIsInRole((roles, ctrl) => { return roles.indexOf(User.role) !== -1; })
-        .map("/", ["guest", "employee", "manager"])
+    AlanAuth.setIsInRole(function(roles, ctrl){
+			return roles.indexOf(User.role) !== -1;
+		}).map("/", ["guest", "employee", "manager"])
         .map("/login", ["guest"], "/personal")
         .map(["/personal", "/logout"], ["employee", "manager"], "/login")     // employee和manager都可以访问/logout和/personal
         .map("/salary", "employee", "/personal")     // 只有employee可以访问/salary, 其他角色访问会跳转到/personal页面, 对于guest角色, 先跳转到/personal, 又从/personal跳转到/login
